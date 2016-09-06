4df98e94d7645b26a3011c6e80fde2563f5831f1
unknown <King Philip@Micro.(none)>
2016-09-05 22:27:09 -0400

    splitting up destructable blocks

--- Assets/Scripts/DestructableBlock.cs
+++ Assets/Scripts/DestructableBlock.cs
@@ -29,35 +29,86 @@ public class DestructableBlock : Destructable {
         }
     }
 
+    struct BlockUnit
+    {
+        public Vector3 Point;
+        public float Distance;
+        public Vector3 Index;
+    }
     public override void OnDestroyed(Collision collision)
     {
         base.OnDestroyed(collision);
         
-        var collisionPoint = transform.InverseTransformVector(collision.contacts[0].point);
+        var collisionPoint = transform.InverseTransformPoint(collision.contacts[0].point);
 
-        List<Vector3> points = new List<Vector3>();
-        for (int x = 0; x < _rbb.Block.Size.x; x++)
+
+        var blockSize = _rbb.Block.InitialSize;
+        List<BlockUnit> points = new List<BlockUnit>();
+        for (int x = 0; x < blockSize.x; x++)
         {
-            for (int y = 0; y < _rbb.Block.Size.y; y++)
+            for (int y = 0; y < blockSize.y; y++)
             {
-                for (int z = 0; z < _rbb.Block.Size.z; z++)
+                for (int z = 0; z < blockSize.z; z++)
                 {
-                    points.Add(new Vector3(
-                        0.5f - _rbb.Block.Size.x / 2 + x,
-                        0.5f - _rbb.Block.Size.y / 2 + y,
-                        0.5f - _rbb.Block.Size.z / 2 + z
-                    ));
+                    Vector3 point = new Vector3(
+                        0.5f - blockSize.x / 2 + x,
+                        0.5f - blockSize.y / 2 + y,
+                        0.5f - blockSize.z / 2 + z
+                    );
+                    points.Add(new BlockUnit {
+                        Point = point,
+                        Distance = Vector3.Distance(point, collisionPoint),
+                        Index = new Vector3(x,y,z)
+                    });
                 }
             }
         }
-        var quad = points.Select(p => new
+        var quad = points.OrderBy(p => p.Distance).First();
+        CreateSmallPieces(quad.Point);
+
+        List<float> position = new List<float> { 0.0f, 0.0f, 0.0f};
+        List<float> size = new List<float> { blockSize.x, blockSize.y, blockSize.z };
+        List<float> index = new List<float> { quad.Index.x, quad.Index.y, quad.Index.z };
+
+        SliceDimension(0, position, size, index);
+        SliceDimension(1, position, size, index);
+        SliceDimension(2, position, size, index);
+    }
+
+    private void SliceDimension(int dimension, List<float> position, List<float> size, List<float> index)
+    {
+        if (size[dimension] > 1)
         {
-            point = p,
-            distance = Vector3.Distance(p, collisionPoint)
-        }).OrderBy(p => p.distance).First();
+            if (index[dimension] > 0.0f) {
 
+                List<float> piecePos = new List<float>(position);
+                piecePos[dimension] = position[dimension] - (0.5f + (index[dimension]) / 2);
+                
+                List<float> pieceSize = new List<float>(size);
+                pieceSize[dimension] = index[dimension];
 
-        CreateSmallPieces(quad.point);
+                CreateLargePiece(piecePos, pieceSize);
+            }
+
+            if (index[dimension] < size[dimension] - 1)
+            {
+                List<float> piecePos = new List<float>(position);
+                piecePos[dimension] = position[dimension] + (0.5f + ((size[dimension] - 1) - index[dimension]) / 2);
+
+                List<float> pieceSize = new List<float>(size);
+                pieceSize[dimension] = (size[dimension] - 1) - index[dimension];
+
+                CreateLargePiece(piecePos, pieceSize);
+            }
+
+            position[dimension] = index[dimension] - size[dimension] / 2 + 0.5f;
+            size[dimension] = 1.0f;
+        }
+    }
+
+    private void CreateLargePiece(List<float> pos, List<float> size)
+    {
+        Debug.Log("creating a large piece");
     }
 
     private void CreateSmallPieces(Vector3 pos)
@@ -78,13 +129,14 @@ public class DestructableBlock : Destructable {
     private void CreateBlockPiece(Vector3 localCenter, Vector3 position)
     {
         var b = Instantiate(SmallBlockPiece, transform.parent) as RealBlockBehavior;
-        
-        
-        b.transform.position = transform.TransformVector(new Vector3(
+
+        var localPosition = new Vector3(
             localCenter.x + (Mathf.Approximately(position.x, 0.0f) ? position.x : position.x > 0 ? position.x - b.Block.Size.x / 2 : position.x + b.Block.Size.x / 2),
             localCenter.y + (Mathf.Approximately(position.y, 0.0f) ? position.y : position.y > 0 ? position.y - b.Block.Size.y / 2 : position.y + b.Block.Size.y / 2),
             localCenter.z + (Mathf.Approximately(position.z, 0.0f) ? position.z : position.z > 0 ? position.z - b.Block.Size.z / 2 : position.z + b.Block.Size.z / 2)
-        ));
+        );
+
+        b.transform.position = transform.TransformPoint(localPosition);
         b.transform.rotation = transform.rotation;
         b.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
     }
